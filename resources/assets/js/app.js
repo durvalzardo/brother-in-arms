18c2712977fb2abf77065f212497e93e3914c285
Lucas Primo <luckasprimo@gmail.com>
2020-04-02 09:13:28 -0300

    UPDATE frontend adjustments

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -10,10 +10,10 @@ $(document).ready(function () {
       location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
       && 
       location.hostname == this.hostname
-    ) {
+      ) {
       // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       // Does a scroll target exist?
       if (target.length) {
         // Only prevent default if animation is actually gonna happen
@@ -36,20 +36,57 @@ $(document).ready(function () {
     }
   });
 
+
+
   $('#btn-apoia-se').on('click', function(e){
     fbq('trackCustom', 'Clique Apoiese Topo');
     gtag('event', 'Clique Apoiese Topo');
   });
 
-  $('#btn-doacao').on('click', function(e){
-    $('#modal-botoes').fadeIn();
 
-    fbq('trackCustom', 'Clique Apoiase Doador');
-    gtag('event', 'Clique Apoiase Doador');
+
+  $('.js-btn-modal').on('click', function(e){
+    var target = $(this).data('target');
+    $(target).fadeIn();
   });
 
+
+
   $('.js-modal-close').on('click', function() {
-    $('#modal-botoes').fadeOut();
+    $(this).closest('.c-modal').fadeOut();
+  });
+
+
+
+  var $body = $('body');
+  $('.js-menu-trigger').on('click', function() {
+    $body.toggleClass('menu-opened');
+  });
+
+
+
+  var $htmlBody = $('html, body');
+  $('.js-anchor').on('click', function() {
+
+    var target = $(this).data('anchor');
+
+    $body.removeClass('menu-opened');
+
+    $htmlBody.animate({
+      scrollTop: $(target).offset().top - 100
+    }, 300);
+  });
+
+
+
+  var $document   = $(document);
+  $document.on('scroll', function(){
+    if ( $(this).scrollTop() >= 80 ){
+      $body.addClass('is-scroll');
+    }
+    else{
+      $body.removeClass('is-scroll');
+    }
   });
 
 
